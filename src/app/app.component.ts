27b0d22ce24d6371f23ae107ff453511fd39ac1d
mahrokh <mahrokh.sadro@j-sas.com>
2024-12-13 22:49:45 -0500

    updating components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,31 @@ import { Component, NgZone, ChangeDetectorRef } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  currentSort: string = 'bubble';  // Holds the current sorting algorithm to be shown
+
+  currentSort: string = 'bubble';
+  arraySize: number = 50;
+  sortType="Bubble Sort";
+  array: number[] = [];
+  sortedValues: number[] = [];
+  isComparingIndex: number[] = [];
+  isSorting: boolean = false;
+  sliderValue: number = 50;
+
+  constructor() {
+    this.generateArray();
+  }
 
   showComponent(sortType: string): void {
     console.log(sortType)
     this.currentSort = sortType;
   }
 
-  arraySize: number = 50;
-
   generateNewArray(): void {
     this.generateArray();
   }
 
   onArraySizeChange(): void {
-    console.log(`Array size changed to: ${this.arraySize}`);
-    // Logic to handle array size changes
+    
   }
 
   sortArray(sortType: string): void {
@@ -29,37 +38,31 @@ export class AppComponent {
       this.isSorting = true;
       this.bubbleSort();
     }
-    if( sortType=="heap"){
+    else if( sortType=="heap"){
       this.isSorting = true;
       this.sortType="Heap Sort";
       this.heapSort(this.array).then(() => {
         this.isSorting = false;
       });
     }
+    else if( sortType=="insert"){
+      this.isSorting = true;
+      this.sortType="Insertion Sort";
+      this.insertionSort();
+    }
+    else if( sortType=="bucket"){
+      this.isSorting = true;
+      this.sortType="Bucket Sort";
+      this.bucketSort(this.array).then(() => {
+        this.isSorting = false;
+      });
+    }
+    else if(sortType=="quick"){
+      this.isSorting = true;
+      this.sortType="Quick Sort";
+     this.quickSort(0, this.array.length - 1);
+    }
   }
-  sortType="Bubble Sort";
-  array: number[] = [];
-  sortedValues: number[] = [];
-  isComparingIndex: number[] = [];
-  isSorting: boolean = false;
-
-  constructor() {
-    this.generateArray();
-    this.startSorting();
-  }
-
-  // generateArray() {
-  //   this.array = [];
-  //   for (let i = 0; i < 50; i++) {
-  //     this.array.push(Math.floor(Math.random() * 100) + 1);
-  //   }
-  //   this.sortedValues = [...this.array];
-  // }
-  
-  // startSorting() {
-  //   this.isSorting = true;
-  //   this.bubbleSort();
-  // }
 
   async bubbleSort() {
     const len = this.array.length;
@@ -69,21 +72,19 @@ export class AppComponent {
       isSorted = true;
 
       for (let i = 0; i < len - 1; i++) {
-        this.isComparingIndex = [i, i + 1]; // Show bars being compared
+        this.isComparingIndex = [i, i + 1];
 
-        // Pause for animation effect
-        await this.sleep(20);
+        await this.sleep(this.sliderValue);
 
         if (this.array[i] > this.array[i + 1]) {
           [this.array[i], this.array[i + 1]] = [this.array[i + 1], this.array[i]];
-          isSorted = false; // If any swaps occur, we're not done sorting
+          isSorted = false;
         }
 
-        this.isComparingIndex = []; // Clear the comparison state
+        this.isComparingIndex = []; 
       }
     }
 
-    // Once sorted, store the sorted array
     this.sortedValues = [...this.array];
     this.isSorting = false;
   }
@@ -96,9 +97,7 @@ export class AppComponent {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-
-  /////////////////////////////////////////////////////////
-  //heapsort
+ /////////////////////////////////////////////////////////// heapsort
   generateArray() {
     this.array = [];
     for (let i = 0; i < this.arraySize; i++) {
@@ -114,31 +113,22 @@ export class AppComponent {
     });
   }
 
-  // Heap Sort Algorithm
   async heapSort(arr: number[]): Promise<void> {
     const n = arr.length;
 
-    // Build max heap
     for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
       await this.heapify(arr, n, i);
     }
 
-    // Extract elements one by one from the heap
     for (let i = n - 1; i > 0; i--) {
-      // Move the current root to the end
       this.swap(arr, 0, i);
       this.isComparingIndex = [0, i];
-      await this.sleep(500);
-
-      // Heapify the reduced heap
+      await this.sleep(10);
       await this.heapify(arr, i, 0);
     }
-
-    // Set the final sorted array
     this.sortedValues = [...arr];
   }
 
-  // Heapify a subtree rooted at index i
   async heapify(arr: number[], n: number, i: number): Promise<void> {
     let largest = i;
     const left = 2 * i + 1;
@@ -155,36 +145,112 @@ export class AppComponent {
     if (largest !== i) {
       this.swap(arr, i, largest);
       this.isComparingIndex = [i, largest];
-      await this.sleep(500);
-
-      // Recursively heapify the affected subtree
+      await this.sleep(100);
       await this.heapify(arr, n, largest);
     }
   }
 
-  // Swap two elements in the array
   swap(arr: number[], i: number, j: number) {
     const temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
   }
 
-  // Check if the element at the given index is being compared
-  // isComparing(index: number): boolean {
-  //   return this.isComparingIndex.includes(index);
-  // }
+///////////////////////////////////////////////////////////insert sort
+async insertionSort() {
+  const len = this.array.length;
 
-  // // Sleep function to introduce a delay for visualization
-  // sleep(ms: number) {
-  //   return new Promise(resolve => setTimeout(resolve, ms));
-  // }
+  for (let i = 1; i < len; i++) {
+    let key = this.array[i];
+    let j = i - 1;
 
+    this.isComparingIndex = [i];
+    await this.sleep(this.sliderValue);
 
+    while (j >= 0 && this.array[j] > key) {
+      this.isComparingIndex = [j, j + 1]; 
+      await this.sleep(this.sliderValue);
 
+      this.array[j + 1] = this.array[j];
+      j = j - 1;
 
+      this.isComparingIndex = []; 
+      await this.sleep(this.sliderValue);
+    }
 
+    this.array[j + 1] = key; 
+  }
 
+  this.sortedValues = [...this.array];
+  this.isSorting = false;
+}
+////////////////////////////////////////////////////////////bucketsort
+  async bucketSort(arr: number[]): Promise<void> {
+    const maxValue = Math.max(...arr);
+    const bucketCount = Math.floor(maxValue / 10) + 1;
+    const buckets: number[][] = Array.from({ length: bucketCount }, () => []);
 
+    for (let i = 0; i < arr.length; i++) {
+      const index = Math.floor(arr[i] / 10);
+      buckets[index].push(arr[i]);
+    }
+
+    for (let i = 0; i < buckets.length; i++) {
+      await this.insertionSort_(buckets[i], i);
+    }
+    const sortedArray: number[] = buckets.flat();
+
+    for (let i = 0; i < sortedArray.length; i++) {
+      arr[i] = sortedArray[i];
+      await this.sleep(this.sliderValue);
+    }
+
+    this.sortedValues = [...arr];
+  }
+
+  async insertionSort_(bucket: number[], bucketIndex: number): Promise<void> {
+    for (let i = 1; i < bucket.length; i++) {
+      let j = i;
+      while (j > 0 && bucket[j] < bucket[j - 1]) {
+        this.isComparingIndex = [j, j - 1];
+        this.swap(bucket, j, j - 1);
+        await this.sleep(this.sliderValue);
+        j--;
+      }
+    }
+    await this.sleep(this.sliderValue);
+  }
+
+  ///////////////////////////////////////////////////////////////// quick
+  async quickSort(low: number, high: number) {
+    if (low < high) {
+      const pivotIndex = await this.partition(low, high);
+      await this.quickSort(low, pivotIndex - 1);
+      await this.quickSort(pivotIndex + 1, high);
+    } else {
+      this.isSorting = false;
+      this.sortedValues = [...this.array];
+    }
+  }
+
+  async partition(low: number, high: number): Promise<number> {
+    const pivot = this.array[high];
+    let i = low - 1;
+
+    for (let j = low; j < high; j++) {
+      this.isComparingIndex = [i, j]; 
+      await this.sleep(this.sliderValue);
+
+      if (this.array[j] < pivot) {
+        i++;
+        [this.array[i], this.array[j]] = [this.array[j], this.array[i]];
+        this.isComparingIndex = []; 
+        await this.sleep(this.sliderValue);
+      }
+    }
+    [this.array[i + 1], this.array[high]] = [this.array[high], this.array[i + 1]];
+    return i + 1;
+  }
 
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
-import { BubbleSortComponent } from './bubble-sort/bubble-sort.component';
-import { QuicksortComponent } from './quicksort/quicksort.component';
-import { InsertionsortComponent } from './insertionsort/insertionsort.component';
-import { SelectionsortComponent } from './selectionsort/selectionsort.component';
-import { RadixsortComponent } from './radixsort/radixsort.component';
-import { MergesortComponent } from './mergesort/mergesort.component';
-import { HeapsortComponent } from './heapsort/heapsort.component';
-import { BucketsortComponent } from './bucketsort/bucketsort.component';
-
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BubbleSortComponent,
-    QuicksortComponent,
-    InsertionsortComponent,
-    SelectionsortComponent,
-    RadixsortComponent,
-    MergesortComponent,
-    HeapsortComponent,
-    BucketsortComponent,
-
-
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
